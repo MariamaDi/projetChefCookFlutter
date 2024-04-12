aba537f1d728c7d96c9719564fe997609c11be74
Diaby <bac.diaby@gmail.com>
2024-04-12 17:16:10 +0200

    partie graphique login

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,126 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: LoginScreen(),
+      debugShowCheckedModeBanner: false,
     );
+  }
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Hello'),
+    );
+  }
+}
+
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  Map<String, dynamic> userData = {
+    'Email': null,
+    'Password': null,
+  };
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        
+      ),
+      
+      body:Form(
+        key:formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/Logo.png'
+            ,height: 145,
+            width: 145,
+            ),
+
+            Padding(padding: EdgeInsets.symmetric(horizontal: 50.0),
+              child:TextFormField(
+                decoration: InputDecoration(
+                  label: Text('Email'), 
+                  labelStyle: TextStyle(
+                    fontFamily: 'Nunito',
+                  )
+                ),
+                controller: emailController,
+                validator: (String? value) {
+                  return value?.isNotEmpty == true && value == 'test@mail.com' ? null : 'Identifiant invalide';
+                },
+                onSaved: (String? value) => userData['Email'] = value,
+              ),
+            ),
+            Padding(padding: EdgeInsets.symmetric(horizontal: 50.0),
+              child:TextFormField(
+                decoration: InputDecoration(
+                  label: Text('Mot de passe'),
+                  labelStyle: TextStyle(
+                    fontFamily: 'Nunito',
+                  )
+                ),
+                controller: passwordController,
+                validator: (String? value) {
+                  return value?.isNotEmpty == true && value == 'test' ? null : 'Mot de passe invalide';
+                },
+                onSaved: (String? value) => userData['Password'] = value,
+              ),
+            ),
+            Padding(padding: EdgeInsets.symmetric(vertical:50.0),
+              child:ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0XFFFFC500),
+                  padding: EdgeInsets.symmetric(vertical:20, horizontal: 40)
+                ),
+                child: const Text(
+                  'Connexion', 
+                    style:TextStyle(
+                      fontFamily: 'Nunito',
+                    )
+                  ),
+                  onPressed: () => formKey.currentState!.validate()
+                    ? {Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Homepage())),
+                    Authentication.isLoggedIn = true
+                    }
+
+                    : print('INVALID'),
+                  
+              ),
+            ),
+
+          ],
+        ),
+
+      )
+    );
+  }
+}
+
+class Authentication {
+  static bool isLoggedIn = false;
+
+
+  static void logIn() {
+    isLoggedIn = true;
+  }
+
+  static void logOut() {
+    isLoggedIn = false;
   }
 }
