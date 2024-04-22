b0cc8b513910e18a29842bd60fffc66f566ae886
dmariama899 <dmariama899@gmail>
2024-04-22 23:44:27 +0200

    Ajout liste pour chaque onglet de la page recette

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-
 void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -99,8 +98,8 @@ class RecipesPage extends StatelessWidget {
                 child: TabBarView(
                   children: [
                     EntreesTab(), // Widget personnalisé pour afficher les entrées
-                    Icon(Icons.lunch_dining), // Contenu pour Plats
-                    Icon(Icons.cake), // Contenu pour Desserts
+                    PlatsTab(), // Contenu pour Plats
+                    DessertsTab(), // Contenu pour Desserts
                   ],
                 ),
               ),
@@ -118,8 +117,33 @@ class Entree {
   final String title;
   final String description;
   final String duration;
-
   Entree(
+      {required this.image,
+      required this.title,
+      required this.description,
+      required this.duration});
+}
+
+class Plat {
+  final String image;
+  final String title;
+  final String description;
+  final String duration;
+
+  Plat(
+      {required this.image,
+      required this.title,
+      required this.description,
+      required this.duration});
+}
+
+class Dessert {
+  final String image;
+  final String title;
+  final String description;
+  final String duration;
+
+  Dessert(
       {required this.image,
       required this.title,
       required this.description,
@@ -135,6 +159,13 @@ class EntreesTab extends StatelessWidget {
       description: 'Une salade riche et fraîche parfaite pour l\'été.',
       duration: '15 min',
     ),
+    Entree(
+      // Nouvelle entrée ajoutée
+      image: 'assets/salade.jpg',
+      title: 'Houmous Lovers',
+      description: 'Houmous savoureux.',
+      duration: '25 min',
+    ),
   ];
 
   @override
@@ -147,6 +178,73 @@ class EntreesTab extends StatelessWidget {
           leading: Image.asset(entree.image, width: 100), // Image à gauche
           title: Text(entree.title),
           subtitle: Text('${entree.description} - ${entree.duration}'),
+          trailing: Icon(Icons.arrow_forward_ios),
+        );
+      },
+    );
+  }
+}
+
+class PlatsTab extends StatelessWidget {
+  final List<Plat> plats = [
+    Plat(
+      image: 'assets/salade.jpg',
+      title: 'Poulet Rôti',
+      description:
+          'Poulet rôti croustillant sur le dessus et juteux à l\'intérieur.',
+      duration: '1h 30 min',
+    ),
+    Plat(
+      image: 'assets/salade.jpg',
+      title: 'Pasta Carbonara',
+      description: 'Pâtes fraîches avec une sauce carbonara crémeuse.',
+      duration: '25 min',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: plats.length,
+      itemBuilder: (context, index) {
+        Plat plat = plats[index];
+        return ListTile(
+          leading: Image.asset(plat.image, width: 100),
+          title: Text(plat.title),
+          subtitle: Text('${plat.description} - ${plat.duration}'),
+          trailing: Icon(Icons.arrow_forward_ios),
+        );
+      },
+    );
+  }
+}
+
+class DessertsTab extends StatelessWidget {
+  final List<Dessert> desserts = [
+    Dessert(
+      image: 'assets/salade.jpg',
+      title: 'Tarte aux Pommes',
+      description: 'Tarte aux pommes classique avec une pâte croustillante.',
+      duration: '50 min',
+    ),
+    Dessert(
+      image: 'assets/salade.jpg',
+      title: 'Cheesecake',
+      description: 'Cheesecake riche et crémeux sur une base de biscuit.',
+      duration: '2h 20 min',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: desserts.length,
+      itemBuilder: (context, index) {
+        Dessert dessert = desserts[index];
+        return ListTile(
+          leading: Image.asset(dessert.image, width: 100),
+          title: Text(dessert.title),
+          subtitle: Text('${dessert.description} - ${dessert.duration}'),
           trailing: Icon(Icons.arrow_forward_ios),
         );
       },
